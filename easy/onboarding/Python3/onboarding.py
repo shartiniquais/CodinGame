a2335bd87627fdf2ce2cd67d54823520f5de5c98
Arnaud Méron <arnaud.meron2004@gmail.com>
2025-03-18 09:56:10 +0100

    added base files

--- easy/onboarding/Python3/onboarding.py
+++
@@ -1,9 +0,0 @@
-# game loop
-while True:
-    enemy_1 = input()  # name of enemy 1
-    dist_1 = int(input())  # distance to enemy 1
-    enemy_2 = input()  # name of enemy 2
-    dist_2 = int(input())  # distance to enemy 2
-
-    # Determine which enemy is closer and print its name
-    print(enemy_1 if dist_1 < dist_2 else enemy_2)
